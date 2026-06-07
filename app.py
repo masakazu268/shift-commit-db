a5a9cd5a63deaa2e04c1aa7b0f8816576c53053c
masakazu268 <dadofakane@gmail.com>
2026-06-07 12:26:09 +0900

    shift-db9-commit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json
 from datetime import datetime, timedelta
 from flask import Flask, render_template, request, send_file, jsonify # pyright: ignore[reportMissingImports]
 import pandas as pd
+import openpyxl
 
 
 app = Flask(__name__)
